bb82e8d23777b1885c670242a14efca2c68c5658
James Okolie <zebedee788@gmail.com>
2023-12-04 15:50:56 +0200

    init commit

---
+++ test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+
+#print ("Hello, World!")
+
+# word = 'word' #word comment
+# print (word)
+
+# '''
+# sentence = "This is a sentence."
+# print (sentence)
+# '''
+
+
+# paragraph = """This is a paragraph. It is
+# made up of multiple lines and sentences"""
+# print (paragraph)
+
+import sys; x = 'foo'; sys.stdout.write(x + '\n');
